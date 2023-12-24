c5ef9ee4023cbe6acc7696d4667be9d14db5bd90
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-12-24 11:12:42 +0100

    calendar

--- assets/eventsData.js
+++ assets/eventsData.js
@@ -79,76 +79,76 @@ export const events = [
   //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Dave_Cooke_nnn5yr.webp',
   //   foodTruck: 'Toasted Goat',
   // },
-  {
-    id: 9,
-    date: '2023-12-14',
-    dateText: 'Thursday 14 Dec',
-    time: '7:00 PM',
-    name: 'Trivia with DJ Todd Skipper',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Todd_Skipper_crzt65.webp',
-    foodTruck: 'Duck & Felicas',
-  },
-  {
-    id: 10,
-    date: '2023-12-15',
-    dateText: 'Friday 15 Dec',
-    time: '6:00 PM',
-    name: 'Tim Smith',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/placeholder_jzdida.webp',
-    foodTruck: 'The Hungry Nomad',
-  },
-  {
-    id: 11,
-    date: '2023-12-16',
-    dateText: 'Saturday 16 Dec',
-    time: '6:00 PM',
-    name: 'Joseph Lanskey',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Joey_Lanskey_jufbnf.webp',
-    foodTruck: 'Whatever Food Truck',
-  },
-  {
-    id: 12,
-    date: '2023-12-17',
-    dateText: 'Sunday 17 Dec',
-    time: '1:00 PM',
-    name: 'Katie Richards',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Katie_Richards_xbdpkb.webp',
-    foodTruck: 'Duck & Felicas',
-  },
-  {
-    id: 13,
-    date: '2023-12-21',
-    dateText: 'Thursday 21 Dec',
-    time: '7:00 PM',
-    name: 'Trivia with DJ Todd Skipper',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Todd_Skipper_crzt65.webp',
-    foodTruck: 'The Toasted Goat',
-  },
-  {
-    id: 14,
-    date: '2023-12-22',
-    dateText: 'Friday 22 Dec',
-    time: '6:00 PM',
-    name: 'Jimmy James',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Jimmy_James_efdiez.webp',
-    foodTruck: 'The Hungry Nomad',
-  },
-  {
-    id: 15,
-    date: '2023-12-23',
-    dateText: 'Saturday 23 Dec',
-    time: '6:00 PM',
-    name: 'Danny Bani',
-    imageUrl:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Danny_Bani_wzh5tt.webp',
-    foodTruck: 'Whatever Food Truck',
-  },
+  // {
+  //   id: 9,
+  //   date: '2023-12-14',
+  //   dateText: 'Thursday 14 Dec',
+  //   time: '7:00 PM',
+  //   name: 'Trivia with DJ Todd Skipper',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Todd_Skipper_crzt65.webp',
+  //   foodTruck: 'Duck & Felicas',
+  // },
+  // {
+  //   id: 10,
+  //   date: '2023-12-15',
+  //   dateText: 'Friday 15 Dec',
+  //   time: '6:00 PM',
+  //   name: 'Tim Smith',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/placeholder_jzdida.webp',
+  //   foodTruck: 'The Hungry Nomad',
+  // },
+  // {
+  //   id: 11,
+  //   date: '2023-12-16',
+  //   dateText: 'Saturday 16 Dec',
+  //   time: '6:00 PM',
+  //   name: 'Joseph Lanskey',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Joey_Lanskey_jufbnf.webp',
+  //   foodTruck: 'Whatever Food Truck',
+  // },
+  // {
+  //   id: 12,
+  //   date: '2023-12-17',
+  //   dateText: 'Sunday 17 Dec',
+  //   time: '1:00 PM',
+  //   name: 'Katie Richards',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Katie_Richards_xbdpkb.webp',
+  //   foodTruck: 'Duck & Felicas',
+  // },
+  // {
+  //   id: 13,
+  //   date: '2023-12-21',
+  //   dateText: 'Thursday 21 Dec',
+  //   time: '7:00 PM',
+  //   name: 'Trivia with DJ Todd Skipper',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Todd_Skipper_crzt65.webp',
+  //   foodTruck: 'The Toasted Goat',
+  // },
+  // {
+  //   id: 14,
+  //   date: '2023-12-22',
+  //   dateText: 'Friday 22 Dec',
+  //   time: '6:00 PM',
+  //   name: 'Jimmy James',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Jimmy_James_efdiez.webp',
+  //   foodTruck: 'The Hungry Nomad',
+  // },
+  // {
+  //   id: 15,
+  //   date: '2023-12-23',
+  //   dateText: 'Saturday 23 Dec',
+  //   time: '6:00 PM',
+  //   name: 'Danny Bani',
+  //   imageUrl:
+  //     'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Copperlode%20Brewery/Danny_Bani_wzh5tt.webp',
+  //   foodTruck: 'Whatever Food Truck',
+  // },
   {
     id: 16,
     date: '2023-12-24',
